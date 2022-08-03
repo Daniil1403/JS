64d51e68f2873a82e8f810cdd117c07e4b06be39
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-08-03 18:43:29 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,20 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display = "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+// const value = 27.5
+// const value = Math.floor (27.5);
 
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display == "none") {
-      x.style.display = "block";
-  } else {
-     x.style.display = "none";
-    }
-}
+// const value = Math.floor(27.5);// Округлення в низ до цілого числа
+// const value = Math.ceil(27.001); // Округлення до першого більшо числа
+// // const value = Math.round(27.5)// Звичайне математичне округлення
+// console.log(typeof value);
+
+// const user = `fgfdg`;
+
+
+
+// let x = 5;
+// x = 10;
+
+// const y = confirm('da');
+// console.log(y);
+
+const y = prompt('da');
+console.log(y)
