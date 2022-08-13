abdebc925585ae738e4313a5de5c2e03a86b8f8c
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-08-13 08:35:56 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,41 @@
+/* Шпаргалка по методам масивів:
+split/join -- перетворює рядок на масив і назад.
+push () -- додає елементи до кінця,
+pop() -- видаляє елемент з кінця,
+shift() -- видаляє елемент із початку,
+unshift() - додає елементи на початок.
+splice(index, deleteIndex, ...arr) -- починаючи з індексу index, видаляє deleteIndex елементів і вставляє arr. 
+slice(start, end) -- створює новий масив, копіюючи елементи з позиції start до end (не включаючи end).*/
+
+/* Шпаргалка з оголошення функцій у JS:
+Функціональний вираз (function expression)
+const greet = function (name) {
+ return `Hello, ${name}`;
+};
+
+Оголошення функції (function declaration)
+function greet(name) {
+ return `Hello, ${name}!`;
+} */
+
+/* Декілька лайфхаків при роботі з масивами.
+Як швидко очистити масив
+const fruits = ['banana', 'apple', 'orange', 'watermelon', 'apple', 'orange', 'grape', 'apple'];
+fruits.length = 0;
+console.log(fruits); // поверне []
+
+Як об’єднати більше двох масивів
+const fruits = ['apple', 'banana', 'orange'];
+const meat = ['poultry', 'beef', 'fish'];
+const vegetables = ['potato', 'tomato', 'cucumber'];
+const food = [...fruits, ...meat, ...vegetables];
+console.log(food); // поверне ["apple", "banana", "orange", "poultry", "beef", "fish", "potato", "tomato", "cucumber"]
+
+Як отримати рандомне значення масиву
+const fruits = [ 'banana', 'apple', 'orange', 'watermelon', 'apple', 'orange', 'grape', 'apple', ];
+const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+console.log(randomFruit); // поверне рандомний фрукт із масиву */
+
 /* Функция, которая принимает массив цен и возвращает их сумму */
 const calculateTotalPrice = function (items) {
  let total = 0;
